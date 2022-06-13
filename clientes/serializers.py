6702eedec1e61ba34527414c0fec44067b3455fe
iMaary <m.rodriguesnave@gmail.com>
2022-06-13 18:21:26 -0300

    serializer validators refactoring

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,30 +1,20 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
 
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError("The name doesn't might has numeric values ")
-        
-        return nome
+    def validate(self, data):
+        if not cpf_is_valid(data['cpf']):
+            raise serializers.ValidationError({'cpf': 'The CPF might to has 11 digits'})
+        if not name_is_valid(data['nome']):
+            raise serializers.ValidationError({'nome': 'The name not might has numeric values'})
+        if not rg_is_valid(data['rg']):
+            raise serializers.ValidationError({'rg': 'The RG might to has 9 digits'})
+        if not cellphone_is_valid(data['celular']):
+            raise serializers.ValidationError({'celular': 'The cellphone might to has 11 digits'})
 
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("The CPF might to has 11 digits")
-        return cpf
-    
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError("The RG might to has 9 digits")
-
-        return rg
-
-    def validate_celular(self, celular):
-        if len(celular) != 11:
-            raise serializers.ValidationError("The cellphone might to has 11 digits")
-        
-        return celular
+        return data
---
+++ clientes/validators.py
@@ -0,0 +1,11 @@
+def cpf_is_valid(cpf):
+    return len(cpf) == 11
+
+def name_is_valid(name):
+        return name.isalpha()
+
+def rg_is_valid(rg):
+    return len(rg) == 9
+
+def cellphone_is_valid(cellphone):
+    return len(cellphone) == 11
